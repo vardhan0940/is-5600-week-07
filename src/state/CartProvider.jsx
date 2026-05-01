7604b3f91b20f474b25198c47ae36f46c3fb74d3
Sai Krishna Vardhan Reddy <109133288+vardhan0940@users.noreply.github.com>
2026-05-01 22:58:10 +0000

    lab-07

--- src/state/CartProvider.jsx
+++ src/state/CartProvider.jsx
@@ -1,25 +1,20 @@
 import React, { useReducer, useContext } from 'react'
-
 // Initialize the context
 const CartContext = React.createContext()
-
 // Definte the default state
 const initialState = {
   itemsById: {},
   allItems: [],
 }
-
 // Define reducer actions
 const ADD_ITEM = 'ADD_ITEM'
 const REMOVE_ITEM = 'REMOVE_ITEM'
 const UPDATE_ITEM_QUANTITY = 'UPDATE_ITEM_QUANTITY'
-
 // Define the reducer
 const cartReducer = (state, action) => {
   const { payload } = action;
   switch (action.type) {
     case ADD_ITEM:
-      console.log({state, action})
       const newState = {
         ...state,
         itemsById: {
@@ -34,6 +29,7 @@ const cartReducer = (state, action) => {
         // Use `Set` to remove all duplicates
         allItems: Array.from(new Set([...state.allItems, action.payload._id])),
       };
+      console.log({ newState })
       return newState
     case REMOVE_ITEM:
       const updatedState = {
@@ -49,44 +45,53 @@ const cartReducer = (state, action) => {
         ),
       }
       return updatedState
-    
+       case UPDATE_ITEM_QUANTITY:
+      const currentItem = state.itemsById[payload._id]
+      const updatedItemState = {
+        ...state,
+        itemsById: {
+          ...state.itemsById,
+          [payload._id]: {
+            ...currentItem,
+            quantity: currentItem.quantity + payload.quantity,
+          },
+        },
+      }
+      return updatedItemState
+
     default:
       return state
   }
 }
-
 // Define the provider
 const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(cartReducer, initialState)
-
   // Remove an item from the cart
   const removeFromCart = (product) => {
     dispatch({ type: REMOVE_ITEM, payload: product })
   }
-
   // Add an item to the cart
   const addToCart = (product) => {
     dispatch({ type: ADD_ITEM, payload: product })
   }
 
   // todo Update the quantity of an item in the cart
-  const updateItemQuantity = (productId, quantity) => {
-    // todo
+   const updateItemQuantity = (product, quantity) => {
+    // todo 
   }
 
   // todo Get the total price of all items in the cart
   const getCartTotal = () => {
     // todo
   }
-
   const getCartItems = () => {
     return state.allItems.map((itemId) => state.itemsById[itemId]) ?? [];
   }
-
   return (
     <CartContext.Provider
       value={{
         cartItems: getCartItems(),
+        allItems: state.allItems,
         addToCart,
         updateItemQuantity,
         removeFromCart,
@@ -97,7 +102,4 @@ const CartProvider = ({ children }) => {
     </CartContext.Provider>
   )
 }
-
-const useCart = () => useContext(CartContext)
-
-export { CartProvider, useCart }
+export { CartProvider, CartContext }
